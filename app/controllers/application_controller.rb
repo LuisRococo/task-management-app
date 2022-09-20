fdc750d7dd263f0d6497ec0cb10d7a0953210b90
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-20 16:34:57 -0700

    remove persona-auth verification from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,4 @@ class ApplicationController < ActionController::Base
   include AuthorizedPersona::Authorization
   
   before_action :authenticate_user!
-
-  authorize_persona class_name: "User"
 end
