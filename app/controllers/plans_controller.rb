535e1e20072c0077b075a1af8243cdf8d430a9f6
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-22 20:15:37 -0700

    Add edit plan page

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,7 +1,17 @@
 class PlansController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :edit]
+  before_action :set_plan, only: [:edit]
 
   def index
     @plans = Plan.all
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_plan
+    @plan = Plan.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
   get 'our-plans', to: 'pages#plans'
-  resources :plans, only: [:index]
+  resources :plans, only: [:index, :edit, :update]
 end
