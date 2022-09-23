427983faeb79c24f6bc1af28531835650608de87
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-22 21:51:55 -0700

    Implement update functionality to plans

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,6 +1,6 @@
 class PlansController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :edit]
-  before_action :set_plan, only: [:edit]
+  skip_before_action :authenticate_user!, only: [:index, :edit, :update]
+  before_action :set_plan, only: [:edit, :update]
 
   def index
     @plans = Plan.all
@@ -9,9 +9,23 @@ class PlansController < ApplicationController
   def edit
   end
 
+  def update
+    if @plan.update(plan_params)
+      flash[:notice] = 'The plan was successfully updated'
+      redirect_to plans_path
+    else
+      flash[:error] = 'There was an error'
+      render 'update'
+    end
+  end
+
   private
 
   def set_plan
     @plan = Plan.find(params[:id])
+  end
+
+  def plan_params
+    params.require(:plan).permit(:title, :member_quantity, :price, :time_months)
   end
 end
