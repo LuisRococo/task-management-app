c63c0b9e2c3414799adc693d1a53fc5242fea4cd
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-25 10:58:46 -0700

    Add delete board feature

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,32 @@
 class BoardsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
+  before_action :set_article, only: [:destroy]
 
   def index
     @boards = current_user.boards
   end
 
+  def destroy
+    board_belongs_to_user = current_user.boards.include?(@board)
+
+    unless board_belongs_to_user
+      flash[:alert] = 'Board does not belongs to the current user'
+    else
+      if @board.destroy
+        flash[:notice] = 'Board was deleted'
+      else
+        flash[:alert] = 'Something went wrong'
+      end
+    end
+
+    redirect_to boards_path, status: :see_other
+  end
+
   def show; end
+
+  private
+
+  def set_article
+    @board = Board.find(params[:id])
+  end
 end
