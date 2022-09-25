ec903dbe3f96987b74c1f9e31636eef4dc6bf8cf
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-25 13:20:43 -0700

    Add task list model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_25_033715) do
+ActiveRecord::Schema.define(version: 2022_09_25_201436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 2022_09_25_033715) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "task_lists", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+    t.integer "priority"
+    t.bigint "board_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_task_lists_on_board_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,4 +63,5 @@ ActiveRecord::Schema.define(version: 2022_09_25_033715) do
   end
 
   add_foreign_key "boards", "users", column: "author_id"
+  add_foreign_key "task_lists", "boards"
 end
