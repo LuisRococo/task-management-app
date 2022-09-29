e7f1dead3710ee78c41f01452a0316f1b94153d5
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-29 14:01:36 -0700

    Add task_user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_29_194854) do
+ActiveRecord::Schema.define(version: 2022_09_29_205137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,15 @@ ActiveRecord::Schema.define(version: 2022_09_29_194854) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["board_id"], name: "index_task_lists_on_board_id"
+  end
+
+  create_table "task_users", force: :cascade do |t|
+    t.bigint "task_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_task_users_on_task_id"
+    t.index ["user_id"], name: "index_task_users_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
