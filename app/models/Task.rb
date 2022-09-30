f76d109158eeb0283607633deaf2a89d03981d0a
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-30 14:04:54 -0700

    Fix error on creating seeds

--- app/models/Task.rb
+++ app/models/Task.rb
@@ -3,7 +3,8 @@ class Task < ApplicationRecord
   belongs_to :task_list
   has_rich_text :content
   has_many :task_change_records
-  before_save :add_change_record if :will_save_change_to_task_list_id?
+  before_update :add_change_record if :will_save_change_to_task_list_id?
+  after_create :add_change_record_on_create
   has_many :task_users
   has_many :users, through: :task_users
 
@@ -15,5 +16,9 @@ class Task < ApplicationRecord
     unless task_list_id == task_list_id_was
       TaskChangeRecord.create(new_list: self.task_list.title, task: self)
     end
+  end
+
+  def add_change_record_on_create
+    TaskChangeRecord.create(new_list: self.task_list.title, task: self)
   end
 end
