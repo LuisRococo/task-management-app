0e6b351d04fa4493d50d5c95e21a2a75f8093528
Luis Roberto <luisrobertopenav@gmail.com>
2022-10-02 16:26:42 -0700

    Fix bug that did not allow to delete boards and task_lists

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,4 @@
 class Board < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :task_lists
+  has_many :task_lists, dependent: :destroy
 end
--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,5 +1,5 @@
 class TaskList < ApplicationRecord
   belongs_to :board
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   alias_attribute :title, :name
 end
