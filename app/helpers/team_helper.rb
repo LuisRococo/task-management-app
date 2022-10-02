1e988f201415af570ea3534d8c3a5fced701d7e2
Luis Roberto <luisrobertopenav@gmail.com>
2022-10-02 12:18:17 -0700

    Add new team page

---
+++ app/helpers/team_helper.rb
@@ -0,0 +1,7 @@
+module TeamHelper
+  def team_new_header_page
+    { title: 'New team member',
+      text: 'Add a new member to you team',
+      config: true }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get 'complete_task/:id', to: 'tasks#complete_task'
   post 'complete_task/:id', to: 'tasks#complete_task_action'
   resources :task_users, only: [:create, :destroy]
+  resources :teams, only: [:new]
 end
