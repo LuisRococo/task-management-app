2778e8a182ac342995b9b93e75decca21cfd5671
Luis Roberto <luisrobertopenav@gmail.com>
2022-10-01 18:01:11 -0700

    Fix sign up error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   root 'pages#home'
   get 'our-plans', to: 'pages#plans'
   resources :plans
