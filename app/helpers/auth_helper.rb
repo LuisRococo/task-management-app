d51f5ae0fd1b1d3af8fc2b35698f31d573902599
Luis Roberto <luisrobertopenav@gmail.com>
2022-10-02 16:21:01 -0700

    Refactor code

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -1,9 +1,13 @@
 module AuthHelper
-  def user_logged
+  def user_logged?
     !!current_user
   end
 
   def user_can_access_team_resources
-    user_logged && current_user.authorization_tier == :manager
+    user_logged? && current_user.authorization_tier == :manager
+  end
+
+  def user_admin?
+    user_logged? && current_user.authorization_tier == :admin
   end
 end
