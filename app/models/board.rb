efe096b6bebc4bf5e5b982c4479f78da64ad252d
Luis Roberto <luisrobertopenav@gmail.com>
2022-10-03 11:27:41 -0700

    Add maximum task list per board validation

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,9 +1,18 @@
 class Board < ApplicationRecord
+  @@TASK_LIST_LIMIT = 50
   belongs_to :author, class_name: 'User'
   has_many :task_lists, dependent: :destroy
 
   def user_has_access?(user)
     manager = user.authorization_tier == 'user' ? user.manager : user
     manager == author
+  end
+
+  def self.TASK_LIST_LIMIT
+    @@TASK_LIST_LIMIT
+  end
+
+  def max_task_lists_reached?
+    task_lists.count >= @@TASK_LIST_LIMIT
   end
 end
