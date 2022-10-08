ad265ef03d514aa974619910471f81037a109c93
Luis Roberto <luisrobertopenav@gmail.com>
2022-10-08 12:41:18 -0700

    Add extra information on payment page

--- app/helpers/payment_helper.rb
+++ app/helpers/payment_helper.rb
@@ -4,4 +4,12 @@ module PaymentHelper
       text: 'Pay you plan and discover the potential of your team.',
       config: false }
   end
+
+  def plan_cost
+    current_user.plan.price.format
+  end
+
+  def plan_name
+    current_user.plan.title.capitalize
+  end
 end
