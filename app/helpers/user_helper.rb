19b6bb8b6165ea49548e44091d1ff3f9a36a49ec
Luis Roberto <luisrobertopenav@gmail.com>
2022-10-09 16:12:50 -0700

    Add random message into boards show page

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -17,4 +17,16 @@ module UserHelper
   def white_listed?(user)
     user.white_listed?
   end
+
+  def random_message
+    messages = [
+      'When you have a dream, you\'ve got to grab it and never let go.',
+      'Nothing is impossible. The word itself says \'I\'m possible!\'',
+      'There is nothing impossible to they who will try.',
+      'The bad news is time flies. The good news is you\'re the pilot.',
+      'Life has got all those twists and turns. You\'ve got to hold on tight and off you go.',
+      'Keep your face always toward the sunshine, and shadows will fall behind you.'
+    ]
+    messages[rand(0...messages.count)]
+  end
 end
